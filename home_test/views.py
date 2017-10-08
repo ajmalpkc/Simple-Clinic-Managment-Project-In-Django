49e864aab509d78894c78433cfaf511ef735d5c6
Ajmal P K C <ajmalpkc@gmail.com>
2017-10-08 18:52:19 +0530

    authentication based on user_type

--- home_test/views.py
+++ home_test/views.py
@@ -7,24 +7,32 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 
 from .forms import SignInForm
+from .models import UserProfile
 
 # doctor
 def dotodaypatient(request):
     return render(request, "doctor/today_patient/today_patient.html",{})
+
 def dotodaypatientdetails(request):
     return render(request, "doctor/today_patient/today_patient_details.html",{})
+
 def dochangepassword(request):
     return render(request, "doctor/common/change_password.html",{})
 
 # receptionist
 def retodaybooking(request):
+
     return render(request, "receptionist/today_booking/today_booking.html",{})	
+
 def doctorlist(request):
     return render(request, "receptionist/doctor_list/doctor_list.html",{})
+
 def redepartment(request):
     return render(request, "receptionist/department/department.html",{})
+
 def readddepartment(request):
     return render(request, "receptionist/department/department_add.html",{})	
+
 def rechangepassword(request):
     return render(request, "receptionist/common/change_password.html",{})
 	
@@ -45,14 +53,13 @@ def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        receptionist=Group.objects.filter(name='receptionist')
-        doctor=Group.objects.filter(name='doctor')
         user = 	authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            if user.groups.filter(name__in=['receptionist']):
+            userprofile = UserProfile.objects.get(user=user)
+            if userprofile.user_type == 'RECEPTIONIST':
                 return HttpResponseRedirect('/home_test/retodaybooking/')
-            elif user.groups.filter(name__in=['doctor']):
+            elif user.UserProfile.user_type == 'DOCTOR':
                 return HttpResponseRedirect('/home_test/dotodaypatient/')
             else:
                 return HttpResponseRedirect('/home_test/patienthome')
